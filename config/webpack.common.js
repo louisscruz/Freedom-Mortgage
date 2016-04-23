db46e0461b4811d1a033225168ef4f633a46a609
louisscruz <louisstephancruz@me.com>
2016-04-22 23:43:37 -0400

    Try adding moment to helper.root exceptions

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -96,7 +96,8 @@ module.exports = {
         exclude: [
           // these packages have problems with their sourcemaps
           helpers.root('node_modules/rxjs'),
-          helpers.root('node_modules/@angular2-material')
+          helpers.root('node_modules/@angular2-material'),
+          helpers.root('node_modules/moment')
         ]
       }
 
