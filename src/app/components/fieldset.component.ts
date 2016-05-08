77c2849e1b810d52a4abff6fef50c4e0056fe8d8
louisscruz <louisstephancruz@me.com>
2016-05-08 18:44:55 -0400

    Clean up fieldset component

--- src/app/components/fieldset.component.ts
+++ src/app/components/fieldset.component.ts
@@ -1,33 +1,13 @@
-import {isPresent} from '@angular/core/src/facade/lang';
 import {
   Component,
   ContentChild,
-  Attribute,
-  ElementRef,
-  Renderer,
-  Optional,
   Input,
-  TemplateRef,
   HostBinding
 } from '@angular/core';
 import {Control, NgFormControl} from '@angular/common';
 
-import {BootstrapInputDirective} from '../directives/input.directive';
-
-/**
-* RouterActive dynamically finds the first element with routerLink and toggles the active class
-*
-* ## Use
-*
-* ```
-* <li router-active="active"><a [routerLink]=" ['/Home'] ">Home</a></li>
-* <li [routerActive]=" activeStringValue "><a [routerLink]=" ['/Home'] ">Home</a></li>
-* ```
-*/
 @Component({
   selector: 'fieldset',
-  directives: [BootstrapInputDirective],
-  //providers: [TemplateRef],
   template: `
     <label class="form-control-label">{{label}}</label>
     <ng-content></ng-content>
@@ -39,20 +19,14 @@ import {BootstrapInputDirective} from '../directives/input.directive';
   `
 })
 export class FieldsetComponent {
-  //private errors: Array<Object>;
-
-  constructor(public el: ElementRef, public renderer: Renderer) {
-    //renderer.setElementClass(el.nativeElement, 'form-control', true);
-    console.log(this.errors);
-  }
-
   @Input()
   label: string;
 
   @Input()
   noSuccess: boolean;
 
-  @ContentChild(NgFormControl) state;
+  @ContentChild(NgFormControl)
+  state: NgFormControl;
 
   @Input() errors: Array<{type: string, message?: string}>;
 
